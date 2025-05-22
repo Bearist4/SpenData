291df8d5f1bb0cb5d16874f826ff2c5f5bea483e
Bearist4 <benjamincaillet14@gmail.com>
2025-05-22 19:13:46 +0200

    Initial Commit

---
+++ SpenDataTests/SpenDataTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpenDataTests.swift
+//  SpenDataTests
+//
+//  Created by Benjamin CAILLET on 22.05.25.
+//
+
+import Testing
+@testable import SpenData
+
+struct SpenDataTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
